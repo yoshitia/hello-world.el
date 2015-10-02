8ef9332508cc748e2d3aee0fba2955ab4d8bef4c
yoshitia <naha.it01@gmail.com>
2015-10-02 09:31:19 +0900

    initial version

---
+++ hello-world.el
@@ -0,0 +1,9 @@
+;;hello-world.el
+
+;;discription: This is plugin for learning emacs plugin the first time.
+
+(defun hello-world ()
+      (interactive)
+      (message "Hello World"))
+
+    (provide 'hello-world)
